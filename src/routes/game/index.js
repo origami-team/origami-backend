1a64fb2c9d1158447355a14d7912c0076d5315f9
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2022-11-14 16:05:48 +0100

    add multiplayer route

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -5,11 +5,13 @@ var GameRouter = express.Router();
 
 const { getGame } = require("./getGame");
 const { getAllGames } = require("./getAllGames");
+const { getAllMultiplayerGames } = require("./getAllMultiplayerGames");
 const { getAllGamesWithLocs } = require("./getAllGamesWithLocs");
 const { postGame } = require("./postGame");
 const { putGame } = require("./putGame");
 
 GameRouter.route("/all").get(getAllGames);
+GameRouter.route("/allmultiplayer").get(getAllMultiplayerGames);
 GameRouter.route("/allwithlocs").get(getAllGamesWithLocs);
 GameRouter.route("/:id").get(getGame);
 GameRouter.route("/").post(
