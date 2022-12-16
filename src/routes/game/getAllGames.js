868b0ac02dea6055984a19db87b889ffea2faf8b
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2022-12-16 20:54:19 +0100

    allow only content-admin to get multiplayer games

--- src/routes/game/getAllGames.js
+++ src/routes/game/getAllGames.js
@@ -6,21 +6,27 @@ const Game = require("../../models/game");
 const getAllGames = async (req, res) => {
   try {
     if ("minimal" in req.query) {
-      // let result = await Game.find().select("name").select("place").select("user").select("isVRWorld").select("isCuratedGame");
+      let result;
+      // allow only content-admin to get multiplayer games
+      if ("contentAdmin" in req.query) {
+        // get all games
+        result = await Game.find().select("name").select("place").select("user").select("isVRWorld").select("isCuratedGame").select("isMultiplayerGame").select("numPlayers");
+      } else {
+        // Get all games except multiplyer ones
+        result = await Game.find({
+          $or: [
+            { "isMultiplayerGame": { $eq: false } },
+            { "isMultiplayerGame": { $eq: undefined } }
+          ]
+        }).select("name").select("place").select("user").select("isVRWorld").select("isCuratedGame");
 
-      // Temporarily update for public users
-      // Get all games except multiplyer ones 
-      let result = await Game.find({
-        $or: [
-          { "isMultiplayerGame": {$eq: false} },
-          { "isMultiplayerGame": {$eq: undefined} }
-        ]
-      }).select("name").select("place").select("user").select("isVRWorld").select("isCuratedGame");
+      }
 
       return res.status(200).send({
         message: "Games (minimal) found successfully.",
         content: result,
       });
+
     } else {
       // Get games data except user id
       let result = await Game.find().select("-user");
